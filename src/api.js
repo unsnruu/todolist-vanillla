992474d45108bf5126c12596213f74969d0f9a4e
unsnruu <unsnruu@gmail.com>
2021-11-10 23:25:41 +0900

    Add getId function

--- src/api.js
+++ src/api.js
@@ -1,21 +1,32 @@
 //Server의 역할을 대신하는 로직을 구현합니다.
+let id = 100;
+function getId() {
+  id++;
+  return id.toString();
+}
 
 const db = {
-  categories: ["Today", "Next Week", "Shopping", "Chores", "(Error)"],
+  categories: ["Today", "Next Week", "Shopping", "Chores", "Error!"],
   todos: {
     Today: [
-      { text: "서류 보내기", created: new Date(), deadline: new Date() },
-      { text: "리액트 스터디" },
-      { text: "과제 끝내기" },
+      { text: "서류 보내기", created: new Date(), deadline: null, id: getId() },
+      { text: "리액트 스터디", id: getId() },
+      { text: "과제 끝내기", id: getId() },
     ],
     "Next Week": [],
-    Shopping: [{ text: "과자" }, { text: "폼 클렌저" }],
-    Chores: [{ text: "강아지 산책 하기" }, { text: "수건 빨래 하기" }],
+    Shopping: [
+      { text: "과자", id: getId() },
+      { text: "폼 클렌저", id: getId() },
+    ],
+    Chores: [
+      { text: "강아지 산책 하기", id: getId() },
+      { text: "수건 빨래 하기", id: getId() },
+    ],
   },
 };
 
 function getMenus() {
-  return new Promise((res, rej) => {
+  return new Promise((res, _) => {
     setTimeout(() => {
       return res(db.categories);
     }, 700);
@@ -34,4 +45,4 @@ function getTodos(menu) {
   });
 }
 
-export { getMenus, getTodos };
+export { getMenus, getTodos, getId };
